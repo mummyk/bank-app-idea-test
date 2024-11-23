ff752eff8dbfc28b7fd3beb356ca99bda041e181
chibuzo ikegwuonu <chibuzoi410@gmail.com>
2024-11-23 13:14:48 +0100

    update test

--- core/urls.py
+++ core/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path("", include("users.urls")),
 ]
 
+# Serve static files during development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                           document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
