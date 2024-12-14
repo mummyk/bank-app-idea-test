8d09ba782c90fbce680a0ea9e4f014b14016d7f7
chibuzo ikegwuonu <chibuzoi410@gmail.com>
2024-12-14 16:36:23 +0100

    Updated dashboard interface

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,10 +9,22 @@ from users.models import Transaction
 # Create your views here.
 
 
+import locale
+
+
 @login_required
 def dashboard(request):
+    # Set the locale for formatting
+    # Use the user's default locale (or specify, e.g., 'en_US.UTF-8')
+    locale.setlocale(locale.LC_ALL, '')
+
     # assuming AccountNumber model has user field
     account, created = AccountNumber.objects.get_or_create(user=request.user)
+
+    # Format the account balance in currency format
+    account_balance = locale.currency(
+        account.account_balance, grouping=True, symbol=False)
+
     # Filter transactions by logged-in user's wallet
     transactions = Transaction.objects.filter(wallet__user=request.user)
 
@@ -26,9 +38,14 @@ def dashboard(request):
     paginator = Paginator(ordered_transactions, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {'title': "Dashboard", 'account_type': account.account_type,
-               'account_currency': account.account_currency, 'account_balance': account.account_balance, 'filter': transaction_filter,
-               'transactions': page_obj, }
+    context = {
+        'title': "Dashboard",
+        'account_type': account.account_type,
+        'account_currency': account.account_currency,
+        'account_balance': account_balance,
+        'filter': transaction_filter,
+        'transactions': page_obj,
+    }
     return render(request, 'dashboard/index.html', context)
 
 
