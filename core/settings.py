97f0b5a5334b24ec1c5360c0728c67f7f549dd51
mummyk <chibuzoi410@gmail.com>
2024-12-21 10:27:55 +0000

    Backup 2024-12-21 10:27:55

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
-
+print('Working')
 ALLOWED_HOSTS = ['*']
 
 
